fb3e6d0d92cc9ea275fc09a810b74e88e61dbb52
Woongoh Cho <whdnddh@naver.com>
2024-06-26 20:21:26 +0900

    debug script

---
+++ train_debug.sh
@@ -0,0 +1,4 @@
+data='N3DV/flame_steak'
+time=$(date "+%Y-%m-%d_%H-%M-%S")
+logdir='debug_flame_steak'
+python train.py --eval -s data/${data} --appearance_dim 0 --iterations 20 -m outputs/${logdir}/$time -r 4 --time_dim 4 --init_time positional_encoding --debug 
